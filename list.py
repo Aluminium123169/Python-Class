ae55f6141c8b77c83be6ea6c43707745f90b09fa
Aluminium123169 <penicillin123@gmail.com>
2020-02-15 12:17:13 +0630

    First Commit

---
+++ list.py
@@ -0,0 +1,23 @@
+programA = ['A', 'B', 'C', 'D', 'E']
+programB = ['a', 'b',, 'c', 'd', 'e']
+programC = programA + programB
+programC 
+ ['A', 'B', 'C', 'D', 'E', 'a', 'b', 'c', 'd', 'e']
+programD = [1, 2, 3, 4, 5]
+programC = programC + programD
+programC[9: ] =[]
+programC[5:9] = programD
+programC
+
+len(programC)
+
+a = [10, 20, 30, 40, 50]
+b = [60, 70, 80, 90, 100]
+c = [110, 120, 130, 140, 150]
+x = [a, b, c]
+x
+
+x[0:2][1]
+x[1:2][0]
+
+x[0][]
---
+++ start.py
@@ -0,0 +1,10 @@
+from turtle import *
+color('red', 'yellow')
+begin_fill()
+white True:
+	forard(200)
+	left(170)
+	if abs(pos()) < 1:
+		break
+end_fill()
+done()
